91b2c299c5aa16a736361f8bd7c58d3d281d17d8
Benkeno <81274961+Benkeno@users.noreply.github.com>
2022-01-11 13:29:52 +0100

    comments added

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,9 +1,9 @@
-#include <Arduino.h>
+#include <Arduino.h> // for use in Arduino IDE uncomment this line
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Hello World !");
+  Serial.begin(115200);                       // initialze the serial com port on the ESP board with the .begin(baudrate) function
+  Serial.print("Hello World !");            // lez print a new line on the serial console with "Hello World" for greeting
 
 }
 
