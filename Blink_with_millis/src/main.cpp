0910280edc0d8450f325744e05886e85282c6e67
Benkeno <81274961+Benkeno@users.noreply.github.com>
2022-01-12 14:53:09 +0100

    Blink every x seconds

--- Blink_with_millis/src/main.cpp
+++ Blink_with_millis/src/main.cpp
@@ -11,7 +11,7 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);                                          // initialze the serial com port on the ESP board with the .begin(baudrate) function
-  Serial.print("Blink every second");                            // print a new line on the serial console with a string "Hello World" for greeting
+  Serial.print("Hello World !");                            // print a new line on the serial console with a string "Hello World" for greeting
   pinMode (ledPin, OUTPUT);                                      // set GPIO PIN 2 as output
 }
 
@@ -21,14 +21,14 @@ void loop() {
   unsigned long currentmillis = millis();                       // starts a timer counting up in millisecs
 
   if (currentmillis - timestamp > interval) {                   // if the counted time - the timestamp (difference) is greater than interval ?
-    if (ledPinState == LOW) {                                   // and if ledPinState is LOW ?
+    if (ledPinState == LOW) {                                   // check if ledPinState is LOW ?
       ledPinState = HIGH;                                       // set ledpinstate to HIGH
       timestamp = currentmillis;                                // timestamp becomes the count of time from currentmillis
       Serial.println("LED is ON");                              // print Info for debugging
     }
     else {                                                      
       if (currentmillis - timestamp > interval) {               // if the counted time - the timestamp (difference) is greater than interval ?
-        if (ledPinState == HIGH) {                              // and if ledPinState is HIGH ?
+        if (ledPinState == HIGH) {                              // check if ledPinState is HIGH ?
           ledPinState = LOW;                                    // set ledpinstate to LOW
           timestamp = currentmillis;                            // timestamp becomes the count of time from currentmillis
           Serial.println("LED is OFF");                         // print Info for debugging
