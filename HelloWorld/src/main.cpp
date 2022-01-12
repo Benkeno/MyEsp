cdcb36127bd5bab302480bb370bfc926d485b992
Benkeno <81274961+Benkeno@users.noreply.github.com>
2022-01-12 07:58:17 +0100

    hello world

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -3,7 +3,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);                       // initialze the serial com port on the ESP board with the .begin(baudrate) function
-  Serial.print("Hello World !");            // lez print a new line on the serial console with "Hello World" for greeting
+  Serial.print("Hello World !");            // lez print a new line on the serial console with a string "Hello World" for greeting
 
 }
 
